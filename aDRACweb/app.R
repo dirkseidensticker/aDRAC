113eba96fb40a8c848309911bfd7c2eea43ff303
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2018-05-18 07:13:53 +0200

    added mapview to shiny app - wip

--- aDRACweb/app.R
+++ aDRACweb/app.R
@@ -1,82 +1,134 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-# see: http://shiny.rstudio.com/gallery/basic-datatable.html
-# see: http://www.r-bloggers.com/getting-data-from-an-online-source/
+# see
+# - [x] https://stackoverflow.com/a/48781819
+# - [ ] https://stackoverflow.com/questions/43679843/r-shiny-how-to-select-a-datatable-page-based-on-a-selected-row-when-the-row-ord/43680607
 
+##############################################################################
+# Libraries
+##############################################################################
 
 library(DT)
 library(shiny)
+library(leaflet)
 library(RCurl)
 
+##############################################################################
+# Data
+##############################################################################
 
 myfile <- getURL('https://raw.githubusercontent.com/dirkseidensticker/aDRAC/master/data/aDRAC.csv', ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
 data <- read.csv(textConnection(myfile), header=T, sep=",")
+data$id <- seq.int(nrow(data))
+str(data)
 
-# Define the overall UI
+##############################################################################
+# UI Side
+##############################################################################
+
 ui <- shinyUI(
   fluidPage(
     titlePanel("aDRAC (Archives des datations radiocarbone d'Afrique centrale)"),
-    
-    # Create a new Row in the UI for selectInputs
-    fluidRow(
-      column(3,
-             selectInput("SITE",
-                         "Site:",
-                         c("All",
-                           unique(as.character(data$SITE))))
-      ),
-      column(3,
-             selectInput("COUNTRY",
-                         "Country:",
-                         c("All",
-                           unique(as.character(data$COUNTRY))))
-      ),
-      column(3,
-             selectInput("MATERIAL",
-                         "Dated Material:",
-                         c("All",
-                           unique(as.character(data$MATERIAL))))
-      ),
-      column(3,
-             selectInput("SOURCE",
-                         "Source:",
-                         c("All",
-                           unique(as.character(data$SOURCE))))
-      )
-    ),
-    # Create a new row for the table.
-    fluidRow(
-      DT::dataTableOutput("table")
+    fluidRow(column(12,
+                    fluidRow(
+                leafletOutput("map")),
+             # fluidRow(
+             #   column(3,
+             #          selectInput("SITE",
+             #                      "Site:",
+             #                      c("All",
+             #                        unique(as.character(data$SITE))))
+             #   ),
+             #   column(3,
+             #          selectInput("FEATURE_DESC",
+             #                      "Feature type:",
+             #                      c("All",
+             #                        unique(as.character(data$FEATURE_DESC))))
+             #   ),
+             #   column(3,
+             #          selectInput("POTTERY",
+             #                      "Associated Pottery Styles:",
+             #                      c("All",
+             #                        unique(as.character(data$POTTERY))))
+             #   ),
+             #   column(3,
+             #          selectInput("MATERIAL",
+             #                      "Dated material:",
+             #                      c("All",
+             #                        unique(as.character(data$MATERIAL))))
+             #   )
+             # ),
+             # Create a new row for the table.
+             fluidRow(
+               DT::dataTableOutput("table")
+             )
+           )
+        )
     )
   )
-)
 
-# Define a server for the Shiny app
+##############################################################################
+# Server Side
+##############################################################################
+
 server <- shinyServer(function(input, output) {
   
-  # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- data
-    if (input$SITE != "All") {
-      data <- data[data$SITE == input$SITE,]
-    }
-    if (input$COUNTRY != "All") {
-      data <- data[data$COUNTRY == input$COUNTRY,]
-    }
-    if (input$MATERIAL != "All") {
-      data <- data[data$MATERIAL == input$MATERIAL,]
-    }
-    if (input$SOURCE != "All") {
-      data <- data[data$SOURCE == input$SOURCE,]
-    }
+  qSub <-  reactive({
     data
-  }))
+  })
+  
+  # table
+  output$table <- DT::renderDataTable({
+    DT::datatable(qSub(), 
+                  selection = "single",
+                  options=list(stateSave = TRUE))
+  })
+  
+  # to keep track of previously selected row
+  prev_row <- reactiveVal()
+  
+  # new icon style
+  my_icon = makeAwesomeIcon(icon = 'flag', markerColor = 'red', iconColor = 'white')
+  
+  observeEvent(input$table_rows_selected, {
+    row_selected = qSub()[input$table_rows_selected,]
+    proxy <- leafletProxy('map')
+    print(row_selected)
+    proxy %>%
+      addAwesomeMarkers(popup = as.character(row_selected$LABNR),
+                        layerId = as.character(row_selected$id),
+                        lng = row_selected$LONG, 
+                        lat = row_selected$LAT,
+                        icon = my_icon)
+    
+    # Reset previously selected marker
+    if(!is.null(prev_row()))
+    {
+      proxy %>%
+        addMarkers(popup = as.character(prev_row()$LABNR), 
+                   layerId = as.character(prev_row()$id),
+                   lng = prev_row()$LONG, 
+                   lat = prev_row()$LAT)
+    }
+    # set new value to reactiveVal 
+    prev_row(row_selected)
+  })
+  
+  # map
+  output$map <- renderLeaflet({
+    qMap <- leaflet(data = qSub()) %>% 
+      addTiles() %>%
+      addMarkers(popup = ~as.character(LABNR), 
+                 clusterOptions = markerClusterOptions())
+    qMap
+  })
+  
+  observeEvent(input$map_marker_click, {
+    clickId <- input$map_marker_click$id
+    dataTableProxy("table") %>%
+      selectRows(which(data$id == clickId)) %>%
+      selectPage(which(input$table_rows_all == clickId) %/% 
+                   input$table_state$length + 1)
+  })
+  
   
 })
 
