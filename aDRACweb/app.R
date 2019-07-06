c2c9dd7af6e1e680b3678baf27209bbd8285000f
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2019-07-06 21:29:13 +0200

    refined dataset for gabon based on clist 2004/5 & sanchez-elipe et al. 2016

--- aDRACweb/app.R
+++ aDRACweb/app.R
@@ -27,37 +27,12 @@ str(data)
 ui <- shinyUI(
   fluidPage(
     titlePanel("aDRAC (Archives des datations radiocarbone d'Afrique centrale)"),
-    fluidRow(column(12,
+    fluidRow(column(3,
                     fluidRow(
-                leafletOutput("map")),
-             # fluidRow(
-             #   column(3,
-             #          selectInput("SITE",
-             #                      "Site:",
-             #                      c("All",
-             #                        unique(as.character(data$SITE))))
-             #   ),
-             #   column(3,
-             #          selectInput("FEATURE_DESC",
-             #                      "Feature type:",
-             #                      c("All",
-             #                        unique(as.character(data$FEATURE_DESC))))
-             #   ),
-             #   column(3,
-             #          selectInput("POTTERY",
-             #                      "Associated Pottery Styles:",
-             #                      c("All",
-             #                        unique(as.character(data$POTTERY))))
-             #   ),
-             #   column(3,
-             #          selectInput("MATERIAL",
-             #                      "Dated material:",
-             #                      c("All",
-             #                        unique(as.character(data$MATERIAL))))
-             #   )
-             # ),
-             # Create a new row for the table.
-             fluidRow(
+                leafletOutput("map")
+                )),
+             column(9, 
+                    fluidRow(
                DT::dataTableOutput("table")
              )
            )
@@ -82,54 +57,16 @@ server <- shinyServer(function(input, output) {
                   options=list(stateSave = TRUE))
   })
   
-  # to keep track of previously selected row
-  prev_row <- reactiveVal()
-  
-  # new icon style
-  my_icon = makeAwesomeIcon(icon = 'flag', markerColor = 'red', iconColor = 'white')
-  
-  observeEvent(input$table_rows_selected, {
-    row_selected = qSub()[input$table_rows_selected,]
-    proxy <- leafletProxy('map')
-    print(row_selected)
-    proxy %>%
-      addAwesomeMarkers(popup = as.character(row_selected$LABNR),
-                        layerId = as.character(row_selected$id),
-                        lng = row_selected$LONG, 
-                        lat = row_selected$LAT,
-                        icon = my_icon)
-    
-    # Reset previously selected marker
-    if(!is.null(prev_row()))
-    {
-      proxy %>%
-        addMarkers(popup = as.character(prev_row()$LABNR), 
-                   layerId = as.character(prev_row()$id),
-                   lng = prev_row()$LONG, 
-                   lat = prev_row()$LAT)
-    }
-    # set new value to reactiveVal 
-    prev_row(row_selected)
-  })
-  
   # map
   output$map <- renderLeaflet({
     qMap <- leaflet(data = qSub()) %>% 
       addTiles() %>%
-      addMarkers(popup = ~as.character(LABNR), 
-                 clusterOptions = markerClusterOptions())
+      addCircles(label = ~as.character(SITE), 
+                 popup = ~as.character(SITE), 
+                 color = ~C14AGE)
     qMap
   })
-  
-  observeEvent(input$map_marker_click, {
-    clickId <- input$map_marker_click$id
-    dataTableProxy("table") %>%
-      selectRows(which(data$id == clickId)) %>%
-      selectPage(which(input$table_rows_all == clickId) %/% 
-                   input$table_state$length + 1)
-  })
-  
-  
+
 })
 
 # Run the application 
